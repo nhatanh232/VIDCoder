38e8740b7bf0dc1e02773374532ad95d29d44fa6
nhatanh2321997 <nhatanh2321997@gmail.com>
2018-12-03 16:38:02 +0700

    QuanlykhoV2Controller.php

---
+++ 281118/blog/app/Http/Controllers/AdminPage/QuanlykhoV2Controller.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\AdminPage;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class QuanlykhoV2Controller extends Controller
+{
+    //
+}
