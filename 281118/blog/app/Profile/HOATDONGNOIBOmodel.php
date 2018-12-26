4fca1f135cf91974b040e6157c726090897b4ee4
nhatanh2321997 <nhatanh2321997@gmail.com>
2018-12-26 15:01:41 +0700

    model

---
+++ 281118/blog/app/Profile/HOATDONGNOIBOmodel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Profile;
+
+use Illuminate\Database\Eloquent\Model;
+
+class HOATDONGNOIBOmodel extends Model
+{
+    protected $table = 'HOATDONGNOIBO';
+    protected $primaryKey = 'Mahoatdong';
+    protected $keyType = 'string';
+}
