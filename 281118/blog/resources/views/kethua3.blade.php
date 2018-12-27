acd6575f066fd2c00580eeeb98f8e607349c6342
nhatanh2321997 <nhatanh2321997@gmail.com>
2018-12-27 16:18:07 +0700

    update music CHBN

--- 281118/blog/resources/views/kethua3.blade.php
+++ 281118/blog/resources/views/kethua3.blade.php
@@ -1,46 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <title>Cơ Hội Bất Ngờ</title>
-          <meta name="csrf-token" content="{{ csrf_token() }}" />
-        <meta name="keywords" content="" />
-		<meta name="description" content="" />
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!--<link rel="shortcut icon" href="PUT YOUR FAVICON HERE">-->
-        
-        <!-- Google Web Font Embed -->
-        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
-        
-        <!-- Bootstrap core CSS -->
-        <link href="css/bootstrap.css" rel='stylesheet' type='text/css'>
+<head>
+    <title>Cơ Hội Bất Ngờ</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--<link rel="shortcut icon" href="PUT YOUR FAVICON HERE">-->
 
-        <!-- Custom styles for this template -->
-        <link href="{{URL::asset('layouts/js/colorbox/colorbox.css')}}"  rel='stylesheet' type='text/css'>
-        <link href="{{URL::asset('layouts/css/templatemo_style.css')}}"  rel='stylesheet' type='text/css'>
+    <!-- Google Web Font Embed -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
 
-        <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel='stylesheet' type='text/css'>
+
+    <!-- Custom styles for this template -->
+    <link href="{{URL::asset('layouts/js/colorbox/colorbox.css')}}"  rel='stylesheet' type='text/css'>
+    <link href="{{URL::asset('layouts/css/templatemo_style.css')}}"  rel='stylesheet' type='text/css'>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <!--[if lt IE 9]>
           <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
           <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-        <![endif]-->
-        
-          <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-   
-   <!-- datatables -->
-<link rel="stylesheet" type="text/css" href="http://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
-<script type="text/javascript" src="http://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
-    <!-- end -->
-<!-- Script include -->
-    @yield('libary')
-<!-- end -->
+      <![endif]-->
+
+      <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+
+      <!-- datatables -->
+      <link rel="stylesheet" type="text/css" href="http://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
+      <script type="text/javascript" src="http://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+      <!-- end -->
+      <!-- Script include -->
+      @yield('libary')
+      <!-- end -->
 
 
-  
-    </head>    
-    <body style="background-image: url('{{URL::asset('images/ny2019-2.jpg')}}');">
-        
-        @yield('body')
+
+  </head>    
+  <body style="background-image: url('{{URL::asset('images/ny2019-2.jpg')}}');">
+    <audio class="music" controls autoplay loop>
+      <source src="{{URL::asset('music/hpny.mp3')}}" type="audio/mpeg">
+      </audio>
+
+      @yield('body')
        <!--  <div class="templatemo-footer" >
             <div class="container">
                 <div class="row">
@@ -92,11 +95,11 @@
         <script src="{{URL::asset('layouts/js/stickUp.min.js')}}"  type="text/javascript"></script>
         <script src="{{URL::asset('layouts/js/colorbox/jquery.colorbox-min.js')}}"  type="text/javascript"></script>
         <script src="{{URL::asset('layouts/js/templatemo_script.js')}}"  type="text/javascript"></script>
-		<!-- templatemo 395 urbanic -->
+        <!-- templatemo 395 urbanic -->
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-           
 
 
-             
-    </body>
-</html>
+
+
+        </body>
+        </html>
