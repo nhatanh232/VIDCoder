45cc7c62b6080ac5199a2f18dd9dd0c513d9aec9
nhatanh2321997 <nhatanh2321997@gmail.com>
2019-03-15 23:54:25 +0700

    update quay so

--- 281118/blog/resources/views/kethua3.blade.php
+++ 281118/blog/resources/views/kethua3.blade.php
@@ -38,7 +38,7 @@
 
 
   </head>    
-  <body style="background-image: url('{{URL::asset('images/br831.png')}}');">
+  <body style="background-image: url('{{URL::asset('images/br1.jpg')}}');">
     <!-- <audio class="music" controls autoplay loop>
       <source src="{{URL::asset('music/hpny.mp3')}}" type="audio/mpeg">
       </audio> -->
