466e8a5dd57885aa7127ccdc417765ea0a671f90
nhatanh2321997 <nhatanh2321997@gmail.com>
2019-03-20 15:09:12 +0700

    update suất ăn

--- 281118/blog/app/Http/Controllers/SuatAn/SuatAnController.php
+++ 281118/blog/app/Http/Controllers/SuatAn/SuatAnController.php
@@ -181,7 +181,8 @@ class SuatAnController extends Controller
         }else {
             $nextDay = $today->addDay(1);
         }        
-        $data = \DB::select(\DB::raw("select d.*,nv.Name,nv.Department from DangKiSuatAn d, NVDKAn nv where Date = '$nextDay' and d.Staff_ID = nv.Staff_ID 
+        $data = \DB::select(\DB::raw("select d.*,nv.Name,nv.Department from DangKiSuatAn d, NVDKAn nv 
+            where Date = '$nextDay' and d.Staff_ID = nv.Staff_ID 
             order by case when nv.Department = N'BGĐ' then 1
             when nv.Department = N'Ban Trợ Lý' then 2
             when nv.Department = N'NS-HC VĐ' then 3
@@ -198,9 +199,9 @@ class SuatAnController extends Controller
             when nv.Department = N'KHÁNH HỘI' then 14
             when nv.Department = N'ZEN phục vụ' then 15
             when nv.Department = N'ZEN Quầy Nước' then 16
-            when nv.Department = N'NH ZEN Tạp vụ' then 17
+            when nv.Department = N'ZEN Tạp vụ' then 17
             when nv.Department = N'ZEN Bếp' then 18
-            when nv.Department = N'Zen Maketing' then 19
+            when nv.Department = N'ZEN Marketing' then 19
             when nv.Department = N'Zen Thu Mua' then 20
             else nv.Department end asc"));
         return $data;
@@ -217,7 +218,8 @@ class SuatAnController extends Controller
         }else {
             $nextDay = $today->addDay(1);
         }
-        $data = \DB::select(\DB::raw("select d.*,nv.Name,nv.Department from DangKiSuatAn d, NVDKAn nv where Date = '$nextDay' and d.Staff_ID = nv.Staff_ID 
+        $data = \DB::select(\DB::raw("select d.*,nv.Name,nv.Department from DangKiSuatAn d, NVDKAn nv 
+            where Date = '$nextDay' and d.Staff_ID = nv.Staff_ID 
             order by case when nv.Department = N'BGĐ' then 1
             when nv.Department = N'Ban Trợ Lý' then 2
             when nv.Department = N'NS-HC VĐ' then 3
@@ -234,9 +236,9 @@ class SuatAnController extends Controller
             when nv.Department = N'KHÁNH HỘI' then 14
             when nv.Department = N'ZEN phục vụ' then 15
             when nv.Department = N'ZEN Quầy Nước' then 16
-            when nv.Department = N'NH ZEN Tạp vụ' then 17
+            when nv.Department = N'ZEN Tạp vụ' then 17
             when nv.Department = N'ZEN Bếp' then 18
-            when nv.Department = N'Zen Maketing' then 19
+            when nv.Department = N'ZEN Marketing' then 19
             when nv.Department = N'Zen Thu Mua' then 20
             else nv.Department end asc"));
         $date = $nextDay->day."-".$nextDay->month."-".$nextDay->year;
@@ -272,7 +274,9 @@ class SuatAnController extends Controller
         }else {
             $nextDay = $today->addDay(1);
         }
-        $data = \DB::select(\DB::raw("select d.*,nv.Name,nv.Department from DangKiSuatAn d, NVDKAn nv where Date = '$nextDay' and d.Staff_ID = nv.Staff_ID 
+        // Thống Kê Theo Ngày
+        $data = \DB::select(\DB::raw("select d.*,nv.Name,nv.Department from DangKiSuatAn d, NVDKAn nv 
+            where Date = '$nextDay' and d.Staff_ID = nv.Staff_ID 
             order by case when nv.Department = N'BGĐ' then 1
             when nv.Department = N'Ban Trợ Lý' then 2
             when nv.Department = N'NS-HC VĐ' then 3
@@ -289,14 +293,36 @@ class SuatAnController extends Controller
             when nv.Department = N'KHÁNH HỘI' then 14
             when nv.Department = N'ZEN phục vụ' then 15
             when nv.Department = N'ZEN Quầy Nước' then 16
-            when nv.Department = N'NH ZEN Tạp vụ' then 17
+            when nv.Department = N'ZEN Tạp vụ' then 17
             when nv.Department = N'ZEN Bếp' then 18
-            when nv.Department = N'Zen Maketing' then 19
+            when nv.Department = N'ZEN Marketing' then 19
             when nv.Department = N'Zen Thu Mua' then 20
             else nv.Department end asc"));
+
+        //Thống Kê Theo Tháng (Tính tới thời điểm hiện tại)
+        $dataThang = \DB::select(\DB::raw("select T2.Company, T1.Chay, T2.Man, T3.Khong from
+            (select nv.Company,d.Type,count(d.Type) as Chay from DangKiSuatAn d, NVDKAn nv where Date 
+            between (SELECT DATEADD(month, DATEDIFF(month, 0, getdate()), 0)) and getdate() and nv.Staff_ID = d.Staff_ID and d.Type = 'C'
+            group by nv.Company,d.Type) as T1
+            full join
+            (select nv.Company,d.Type,count(d.Type) as Man from DangKiSuatAn d, NVDKAn nv where Date 
+            between (SELECT DATEADD(month, DATEDIFF(month, 0, getdate()), 0)) and getdate() and nv.Staff_ID = d.Staff_ID and d.Type = 'M'
+            group by nv.Company,d.Type) as T2 on T1.Company = T2.Company
+            full join
+            (select nv.Company,d.Type,count(d.Type) as Khong from DangKiSuatAn d, NVDKAn nv where Date 
+            between (SELECT DATEADD(month, DATEDIFF(month, 0, getdate()), 0)) and getdate() and nv.Staff_ID = d.Staff_ID and d.Type = 'O'
+            group by nv.Company,d.Type) as T3 on T2.Company = T3.Company
+            order by case
+            when T1.Company = N'Viễn Đông' then 1
+            when T1.Company = N'Toàn Lực' then 2
+            when T1.Company = N'Hồn Việt' then 3
+            when T1.Company = N'Proci' then 4
+            when T1.Company = N'Khánh Hội' then 5
+            when T1.Company = N'Nhà hàng Zen' then 6
+            else T1.Company end asc"));
         
         $date = $nextDay->day."-".$nextDay->month."-".$nextDay->year;
-        Excel::create('SuatAn_'.$date,function($excel) use($data, $nextDay){
+        Excel::create('SuatAn_'.$date,function($excel) use($data, $nextDay, $dataThang){
             $excel->sheet('Suất Ăn',function($sheet) use($data, $nextDay){
                 $sheet->mergeCells('A1:E1');
                 $sheet->row(1, function ($row) {
@@ -323,6 +349,47 @@ class SuatAnController extends Controller
                 $i = 1;      
                 for($i; $i<=sizeof($data);$i++){
                     $sheet->row($i+3,array($i, $data[$i-1]->Name, $data[$i-1]->Department, $data[$i-1]->Type ,''));
+                    $sheet->setBorder('A3:E'.($i+3), 'thin');
+                }
+            });
+            $excel->sheet('Thống Kê',function($sheet) use($dataThang, $nextDay){
+                $sheet->mergeCells('A1:E1');
+                $sheet->row(1, function ($row) {
+                    $row->setFontSize(24);
+                    $row->setFontWeight('bold');
+                    $row->setValignment('center');
+                    $row->setAlignment('center');
+                });
+                $sheet->row(1,array('THỐNG KÊ SUẤT ĂN'));
+                $sheet->mergeCells('A2:E2');
+                $sheet->row(2, function ($row) {
+                    $row->setFontSize(12);
+                    $row->setFontWeight('bold');
+                    $row->setValignment('center');
+                    $row->setAlignment('center');
+                });
+                $sheet->row(2,array('Tính đến: Ngày '.$nextDay->day.' Tháng '.$nextDay->month.' Năm'.$nextDay->year));
+                $sheet->row(3, function ($row) {
+                    $row->setFontWeight('bold');
+                    $row->setValignment('center');
+                    $row->setAlignment('center');
+                });
+                $sheet->row(3,array('STT', 'Công Ty', 'Mặn', 'Chay' ,'Không')); 
+                $i = 1;      
+                for($i; $i<=sizeof($dataThang);$i++){
+                    $man = $dataThang[$i-1]->Man;
+                    $chay = $dataThang[$i-1]->Chay;
+                    $khong = $dataThang[$i-1]->Khong;
+                    if($man == null){
+                        $man = 0;
+                    }
+                    if($chay == null){
+                        $chay = 0;
+                    }
+                    if($khong == null){
+                        $khong = 0;
+                    }
+                    $sheet->row($i+3,array($i, $dataThang[$i-1]->Company, $man, $chay ,$khong));
                     $sheet->setBorder('A3:E'.($i+3), 'thin');
                 }
             });
