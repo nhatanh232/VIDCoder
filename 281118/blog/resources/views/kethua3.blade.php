9cf31f4f2bace00c2109bae4ca038b7edfb4b495
nhatanh2321997 <nhatanh2321997@gmail.com>
2019-05-03 12:50:09 +0700

    update background CHBN

--- 281118/blog/resources/views/kethua3.blade.php
+++ 281118/blog/resources/views/kethua3.blade.php
@@ -38,7 +38,7 @@
 
 
   </head>    
-  <body style="background-image: url('{{URL::asset('images/chbn1204.jpg')}}');">
+  <body style="background-image: url('{{URL::asset('images/bg05.jpg')}}');">
     <!-- <audio class="music" controls autoplay loop>
       <source src="{{URL::asset('music/hpny.mp3')}}" type="audio/mpeg">
       </audio> -->
